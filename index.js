3848bf296fc2b830553d63bafb3874de31d07806
123game <123game.123game.123game@gmail.com>
2016-03-28 11:06:24 +0700

    update README.md, index.js

--- index.js
+++ index.js
@@ -6,84 +6,112 @@ var rc;
 module.exports = {
 
   connect(): function(port, host, options, callback) {
-    var host = host || "localhost";
-    var port = port || 6379;
-    var options = options || {};
+      var host = host || "localhost";
+      var port = port || 6379;
+      var options = options || {};
 
-    if (options.password) {
-      options.no_ready_check = true;
-    }
-
-    rc = redis.createClient(port, host, options);
-
-    rc.on('connect', function() {
-      console.log('Connect to redis: %s:%d', host, port);
-      if (options && options.password) {
-        rc.auth(options.password, function(err) {
-          if (callback) {
-            callback(err);
-          }
-        });
-      } else {
-        if (callback) {
-          callback(null);
-        }
+      if (options.password) {
+        options.no_ready_check = true;
       }
-    });
-  },
 
-  getProfile: function(device_id, facebook_id, callback) {
-    getUserId(device_id, facebook_id, function(user_id) {
-      if (rc) {
+      rc = redis.createClient(port, host, options);
+
+      rc.on('connect', function() {
+
+        console.log('Connect to redis: %s:%d', host, port);
+
+        if (options.password) {
+          rc.auth(options.password, function(err) {
+            if (callback) {
+              callback(err);
+            }
+          });
+        } else {
+          if (callback) {
+            callback(null);
+          }
+        }
+      });
+
+    },
+
+    getProfile: function(device_id, facebook_id, callback) {
+
+      if (!rc) {
+        callback("Error: not connect to redis");
+        return;
+      }
+
+      getUserId(device_id, facebook_id, function(err, user_id) {
+
+        if (err) {
+          callback(err);
+          return;
+        }
+
         rc.hgetall(user_id, function(err, object) {
           callback(err, object);
         });
-      } else {
+
+      });
+
+    },
+
+    setProfile: function(device_id, facebook_id, object, callback) {
+
+      if (!rc) {
         callback("Error: not connect to redis");
+        return;
       }
-    });
-  },
 
-  setProfile: function(device_id, facebook_id, object) {
-    if (object) {
-      getUserId(device_id, facebook_id, function(user_id) {
-        rc.hmset(user_id, object);
+      getUserId(device_id, facebook_id, function(err, user_id) {
+
+        if (err) {
+          callback(err);
+          return;
+        }
+
+        rc.hmset(user_id, object, function(err, res) {
+          callback(err);
+        });
       });
-    }
-  },
 
-  getUserId: function(device_id, facebook_id, callback) {
-    var user_id;
-    if (facebook_id) {
-      rc.get(facebook_id, function(err, reply) {
-        user_id = reply;
-        if (user_id) {
-          rc.set(device_id, user_id);
-          callback(err, user_id);
-        } else {
-          rc.get(device_id, function(err, reply) {
-            user_id = reply;
-            if (user_id) {
-              rc.set(facebook_id, user_id);
-            } else {
-              user_id = uuid.v1();
-              rc.set(device_id, user_id);
-              rc.set(facebook_id, user_id);
-            }
+    },
+
+    getUserId: function(device_id, facebook_id, callback) {
+
+      if (!rc) {
+        callback("Error: not connect to redis");
+        return;
+      }
+
+      if (facebook_id) {
+        rc.get(facebook_id, function(err, user_id) {
+          if (user_id) {
+            rc.set(device_id, user_id);
             callback(err, user_id);
-          });
-        }
-      });
-    } else {
-      rc.get(device_id, function(err, reply) {
-        user_id = reply;
-        if (!user_id) {
-          user_id = uuid.v1();
-          rc.set(device_id, user_id);
-        }
-        callback(err, user_id);
-      });
+          } else {
+            rc.get(device_id, function(err, user_id) {
+              if (user_id) {
+                rc.set(facebook_id, user_id);
+              } else {
+                user_id = uuid.v1();
+                rc.set(device_id, user_id);
+                rc.set(facebook_id, user_id);
+              }
+              callback(err, user_id);
+            });
+          }
+        });
+      } else {
+        rc.get(device_id, function(err, user_id) {
+          if (!user_id) {
+            user_id = uuid.v1();
+            rc.set(device_id, user_id);
+          }
+          callback(err, user_id);
+        });
+      }
     }
-  }
 
 };
